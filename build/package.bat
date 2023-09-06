82f87bb7a1e1ab00d73f27ec3f5b6c0ce949254c
Travis Bulford <travis@inkusgames.com>
2023-09-06 19:32:15 +0200

    Some adjustments to build scripts

--- build/package.bat
+++ build/package.bat
@@ -1,2 +1,9 @@
 copy ..\app\* ..\target\*
-"c:\Program Files\Java\jdk-19\bin\jpackage" --name Infomancer --input ../target/ --main-jar infomancerforge-0.0.1-SNAPSHOT.jar --type exe --jlink-options --bind-services --vendor "Inkus Games" --icon if_icon.ico --win-shortcut --win-menu --win-menu-group "Inkus" 
+"c:\Program Files\Java\jdk-19\bin\jpackage" --name Infomancer  --app-version 1.0.0.0 --input ../target/ --main-jar infomancerforge-0.0.1-SNAPSHOT.jar --type exe --jlink-options --bind-services --vendor "Inkus Games" --icon if_icon.ico --win-shortcut --win-menu --win-menu-group "Inkus"
+
+rmdir /s .\InfomancerForge
+mkdir .\InfomancerForge
+mkdir .\InfomancerForge\dependency
+copy ..\app\* .\InfomancerForge\*
+copy ..\target\*.jar .\InfomancerForge\*
+copy ..\target\dependency\* .\InfomancerForge\dependency\*
